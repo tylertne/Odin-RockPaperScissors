b197616faa98fb02a607634370f9bd7cadfaee3d
Tyler Neuschatz <tylertne@gmail.com>
2024-03-06 11:46:24 -0800

    create function to generate computer choice
    create a variable that holds a randomly generated number and a function that
    returns either
    rock, paper, or scissors randomly based on that number

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-console.log('hello world')
+//commit early and often
+//Work through with pseudocode and test each part before moving to the next part
+
+//generate a random number between 0 and 1
+//if the number is less than 0.33, output rock
+//if the number is between 0.33 and 0.667 output paper
+//if the number is greater than 0.667 output scissors
+//return the output
+
+const computerOutputNumber = Math.random()
+
+function getComputerChoice(){
+    if(computerOutputNumber < 0.3333){
+        return 'Rock'
+    }
+    else if(computerOutputNumber < 0.6667){
+        return 'Paper'
+    }
+    else{
+        return 'Scissors'
+    }
+}
