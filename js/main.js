0a355fbcf80988c05249a8683f9255a2f1cd21eb
Tyler Neuschatz <tylertne@gmail.com>
2024-03-06 12:12:31 -0800

    add function to play 1 round of RPS
    Add function that takes in a player choice
    and compares to the randomized computer selection
    declaring a winner or a tie and the choices
    each side made.

--- js/main.js
+++ js/main.js
@@ -20,3 +20,31 @@ function getComputerChoice(){
         return 'Scissors'
     }
 }
+
+//inputs are playerSelection (case insensative) and Computer Selection
+//outputs are a string that declares the winner of the round or a tie
+
+//create a function that takes in a case insensative player selection and computer selection
+//if the player selection is rock and the computer seleciton is scissors, or if player selection is paper and the computer selection is rock, or if the player selection is scissors and the computer selection is paper, return 'You win' and the choice that beat the computers choice
+//if the player selection is the same as the computer selection, return 'tie!'
+//if the combination is anything player losing, return 'you lose' and the choice that loses to the computers choice
+//if anything else is entered, that is invalid
+
+function playRound(playerSelection, computerSelection){
+    const playerChoice = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase()
+    if(playerChoice === 'Rock' && computerSelection === 'Scissors' || playerChoice === 'Paper' && computerSelection === 'Rock' || playerChoice === 'Scissors' && computerSelection === 'Paper'){
+        return `You win! ${playerChoice} beats ${computerSelection}`
+    }
+    else if(playerChoice === 'Rock' && computerSelection === 'Paper' || playerChoice === 'Paper' && computerSelection === 'Scissors' || playerChoice === 'Scissors' && computerSelection === 'Rock'){
+        return `You lose! ${computerSelection} beats ${playerChoice}`
+    }
+    else if(playerChoice === computerSelection){
+        return `Tie! ${playerChoice} and ${computerSelection} are the same.`
+    }
+    else{
+        return 'Invalid Input'
+    }
+}
+const playerSelection = "sCisSors";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
